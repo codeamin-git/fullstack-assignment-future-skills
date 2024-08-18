fd3c5870fdc1ba6fc787984dfe26dee92464fd73
codeamin11 <legacyofalamin@gmail.com>
2024-08-18 23:28:49 +0600

    add: server api made

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { connectDB } = require('./config/db');
+const cardRoutes = require('./routes/cardRoutes');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get('/ping', (req, res) => res.send('Help Center is open for help!'));
+app.use(cardRoutes);
+
+// Connect to the database
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Help Center is helping on port ${PORT}`);
+  });
+});
