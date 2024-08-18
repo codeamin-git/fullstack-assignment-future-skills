cf5b8b0bb7cfdf1e829c33eb8aaf77954bfe1a02
codeamin11 <legacyofalamin@gmail.com>
2024-08-18 23:43:27 +0600

    update: card model added with connected db

---
+++ backend/config/db.js
@@ -0,0 +1,21 @@
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function connectDB() {
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.error("Could not connect to MongoDB", error);
+        process.exit(1);
+    }
+}
+
+function getDB() {
+    return client.db('helpCenter');
+}
+
+module.exports = { connectDB, getDB };
---
+++ backend/models/cardModel.js
@@ -0,0 +1,20 @@
+const { getDB } = require('../config/db');
+
+const collection = () => getDB().collection('cards');
+
+// Create a card
+async function createCard(card) {
+  return await collection().insertOne(card);
+}
+
+// Retrieve all cards
+async function getAllCards() {
+  return await collection().find({}).toArray();
+}
+
+// Get a specific card by title
+async function getCardByTitle(title) {
+  return await collection().findOne({ title });
+}
+
+module.exports = { createCard, getAllCards, getCardByTitle };
