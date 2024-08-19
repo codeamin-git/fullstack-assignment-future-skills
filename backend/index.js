8d15a54795eacacea3388dedb2bcdcd19bd83263
codeamin11 <legacyofalamin@gmail.com>
2024-08-19 06:12:08 +0600

    add: client side vercel.json added

--- backend/index.js
+++ backend/index.js
@@ -9,14 +9,14 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: ['https://frontend-chi-brown-61.vercel.app', 'http://localhost:5173', 'http://localhost:5174'],
   credentials: true,
   optionSuccessStatus: 200,
 }
 app.use(cors(corsOptions))
 app.use(express.json());
 
-app.get('/', async(req, res) => res.send('Welcome to Abstract Help Center!'))
+app.get('/', async (req, res) => res.send('Welcome to Abstract Help Center!'))
 
 // Routes
 app.get('/ping', (req, res) => res.send('Help Center is open for help!'));
